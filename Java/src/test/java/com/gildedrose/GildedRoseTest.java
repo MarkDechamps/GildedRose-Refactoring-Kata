4c19dc7b4c400e5312d1a4f577b179cd2f0f9d02
mrxjn <mark.dechamps@gmail.com>
2019-10-06 16:32:27 +0200

    refactor tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,32 +7,51 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void theNameIsPreserved() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-        assertEquals(0, app.items[0].quality);
-        assertEquals(-1, app.items[0].sellIn);
+    public void the_name_doesnt_change() {
+        Item item = new Item("foo", 0, 0);
+        updateQualityFrom(item);
+        assertEquals("the name doesn't change by update","foo",item.name);
     }
+
+    private GildedRose createApp(Item item) {
+        Item[] items = new Item[]{item};
+        return new GildedRose(items);
+    }
+
+    @Test
+    public void the_quality_cant_be_negative() {
+        Item item = new Item("foo", 0, 0);
+        GildedRose app = createApp(item);
+        assertEquals(0, app.items[0].quality);
+        app.updateQuality();
+        assertEquals("the quality can't be negative",0,item.quality);
+    }
+    @Test
+    public void sellin_days_can_be_negative() {
+        Item item = new Item("foo", 0, 0);
+        updateQualityFrom(item);
+        assertEquals("sellin days can be negative",-1, item.sellIn);
+    }
+
+    private void updateQualityFrom(Item item) {
+        GildedRose app = createApp(item);
+        app.updateQuality();
+    }
+
 
     @Test
     public void once_sell_date_is_past_quality_degrades_twice_as_fast(){
         Item pastSelldate = createItemPastSellDate();
         int expectedQuality = pastSelldate.quality - 2;
-        Item[] items = new Item[] { pastSelldate };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(pastSelldate.quality,expectedQuality);
+        updateQualityFrom(pastSelldate);
+        assertEquals("once sell date is past quality degrades twice as fast",pastSelldate.quality,expectedQuality);
     }
     @Test
     public void if_sell_date_is_not_past_quality_degrades_by_one(){
         Item pastSelldate = createItem();
         int expectedQuality = pastSelldate.quality - 1;
-        Item[] items = new Item[] { pastSelldate };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(pastSelldate.quality,expectedQuality);
+        updateQualityFrom(pastSelldate);
+        assertEquals("if sell date is not past, quality degrades by one",pastSelldate.quality,expectedQuality);
     }
 
     private Item createItem() {
