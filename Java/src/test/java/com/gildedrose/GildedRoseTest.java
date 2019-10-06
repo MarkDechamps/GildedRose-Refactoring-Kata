0f7699dd0a6fcd056e4ae9fd734760b0ffe7f975
mrxjn <mark.dechamps@gmail.com>
2019-10-06 15:45:09 +0200

    Add tests for: "Once the sell by date has passed, Quality degrades twice as fast"

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,7 +12,32 @@ public class GildedRoseTest {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
-
+        assertEquals("foo", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    public void once_sell_date_is_past_quality_degrades_twice_as_fast(){
+        Item pastSelldate = createItemPastSellDate();
+        Item[] items = new Item[] { pastSelldate };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(pastSelldate.quality,8);
+    }
+    @Test
+    public void if_sell_date_is_not_past_quality_degrades_by_one(){
+        Item pastSelldate = createItem();
+        Item[] items = new Item[] { pastSelldate };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(pastSelldate.quality,9);
+    }
+
+    private Item createItem() {
+        return new Item("tst",1,10);
+    }
+
+    private Item createItemPastSellDate() {
+        return new Item("tst",0,10);
+    }
 }
